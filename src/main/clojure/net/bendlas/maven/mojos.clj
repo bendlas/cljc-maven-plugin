705eca15572c16ba3330ec96461a4819eead2a22
Herwig Hochleitner <herwig@bendlas.net>
2016-12-25 04:56:27 +0100

    0.1.2: implement split-tests goal

--- src/main/clojure/net/bendlas/maven/mojos.clj
+++ src/main/clojure/net/bendlas/maven/mojos.clj
@@ -18,13 +18,28 @@
  :extends org.apache.maven.plugin.AbstractMojo
  :prefix "split-")
 
-(defn split-execute [this]
+(gen-class
+ :name
+ ^{Mojo    {:name "split-tests"}
+   Execute {:goal  "split-tests"
+            :phase LifecyclePhase/GENERATE_TEST_SOURCES}}
+ net.bendlas.maven.mojos.SplitTestsMojo
+ :extends org.apache.maven.plugin.AbstractMojo
+ :prefix "split-tests-")
+
+(defn run-split! [this source-dir]
   (let [project (.get (.getPluginContext this) "project")]
     (try
       (cljsee-compile
-       [{:source-paths [(str (io/file (.getBasedir project) "src/main/clojure"))]
+       [{:source-paths [(str (io/file (.getBasedir project) source-dir))]
          :output-path (.. project getBuild getOutputDirectory)
          :rules :clj}])
       (catch Exception e
-        (log/error e "During split")
+        (log/error e "During split of" source-dir)
         (throw e)))))
+
+(defn split-execute [this]
+  (run-split! this "src/main/clojure"))
+
+(defn split-tests-execute [this]
+  (run-split! this "src/test/clojure"))
